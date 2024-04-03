aa4160e8df58ffe89285d437c5304c6746b47395
Artur <artur.brandi@rwsretail.com>
2024-04-03 10:42:07 -0300

    update

--- s3Configuration.bash
+++ s3Configuration.bash
@@ -1,49 +1,44 @@
 #!/bin/bash
 
 # Check if bucket name, access key, and secret key are provided as arguments
-if [ $# -ne 4 ]; then
-    echo "Usage: $0 <bucket_name> <access_key> <secret_key> <username>"
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 <bucket_name> <access_key> <secret_key>"
     exit 1
 fi
-
-# Perform system update and upgrade
-echo "Performing system update and upgrade"
-sudo apt update && sudo apt -y upgrade
 
 # Assign parameters
 bucket_name=$1
 access_key=$2
 secret_key=$3
-username=$4
+
+# Preparing ubuntu
+echo "Performing system update and upgrade"
+sudo apt update && sudo apt -y upgrade
 
 # Step 1: Installing s3fs
-echo "installing s3fs"
+echo "Installing s3fs"
 sudo apt -y install s3fs
 
 # Step 2: Creating directories
 echo "Creating directories"
-sudo mkdir -p /var/s3/$username
+sudo mkdir -p /var/s3
 
 # Step 3: Editing shells
 echo "Editing shells"
-if ! grep -q "/bin/false" /etc/shells ; then
-    echo "/bin/false doesn't exist"
-    sudo bash -c 'echo "/bin/false" >> /etc/shells'
-fi
+sudo bash -c 'echo "/bin/false" >> /etc/shells'
 
 # Step 4: Storing AWS accessKey
 echo "Storing AWS accessKey"
-if [ ! -f /etc/passwd-s3fs ]; then
-    echo "$access_key:$secret_key" | sudo tee /etc/passwd-s3fs > /dev/null
-    # Step 5: Adjusting permissions
-    echo "Adjusting permissions"
-    sudo chmod 600 /etc/passwd-s3fs
-fi
+echo "$access_key:$secret_key" | sudo tee /etc/passwd-s3fs > /dev/null
+
+# Step 5: Adjusting permissions
+echo "Adjusting permissions"
+sudo chmod 600 /etc/passwd-s3fs
 
 # Step 6: Configuring S3 Bucket
 echo "Configuring S3 Bucket"
-sudo s3fs $bucket_name -o use_cache=/tmp -o allow_other -o mp_umask=022 -o multireq_max=5 -o nonempty /var/s3/$username
+sudo s3fs $bucket_name -o use_cache=/tmp -o allow_other -o mp_umask=022 -o multireq_max=5 /var/s3
 
 # Step 7: Automating s3fs to start with the machine
 echo "Automating s3fs to start with the machine"
-sudo bash -c "echo \"$bucket_name /var/s3/$username fuse.s3fs  _netdev,use_cache=/tmp,allow_other,mp_umask=022,multireq_max=5,nonempty,passwd_file=/etc/passwd-s3fs   0 0\" >> /etc/fstab"
+sudo bash -c "echo \"$bucket_name /var/s3 fuse.s3fs  _netdev,use_cache=/tmp,allow_other,mp_umask=022,multireq_max=5,passwd_file=/etc/passwd-s3fs   0 0\" >> /etc/fstab"
