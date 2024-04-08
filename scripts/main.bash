e9576c9776583a6de5846a11f78704cbe2174dbb
Artur <artur.brandi@rwsretail.com>
2024-04-08 17:38:53 -0300

    Update main.bash

--- scripts/main.bash
+++ scripts/main.bash
@@ -10,19 +10,19 @@ validateVariables() {
 setNewEc2() {
   ## Configure machine and install basic software
   sudo chmod +x ./bashTests/scripts/initial_setup.bash
-  ./bashTests/scripts/initial_setup.bash
+  ~/bashTests/scripts/initial_setup.bash
 
   ## S3FS installation and setup
   sudo chmod +x ./bashTests/scripts/s3fs_config.bash
-  ./bashTests/scripts/s3fs_config.bash
+  ~/bashTests/scripts/s3fs_config.bash
 
   ## mount bucket
   sudo chmod +x ./bashTests/scripts/mount_s3fs_bucket.bash
-  ./bashTests/scripts/mount_s3fs_bucket.bash
+  ~/bashTests/scripts/mount_s3fs_bucket.bash
 
   ## create user
   sudo chmod +x ./bashTests/scripts/create_users.bash
-  ./bashTests/scripts/create_users.bash
+  ~/bashTests/scripts/create_users.bash
 }
 
 main() {
