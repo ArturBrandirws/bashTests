ea7b98f16a440feea561d7f3d959458bcdf5353c
Artur <artur.brandi@rwsretail.com>
2024-04-08 19:11:22 -0300

    Update users.bash

--- scripts/users.bash
+++ scripts/users.bash
@@ -1,8 +1,9 @@
 usersList() {
   USER1="rws-integration-service,bucket-demonstracao-2;"
   USER2="rappi,bucket-demonstracao-2;"
+  USER3="teste,bucket-demonstracao-1;"
 
-  userList=($USER1$USER2)
+  userList=($USER1$USER2$USER3)
 
   echo "${userList[@]}"
 }
