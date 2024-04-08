b2782db780cadf7aa33825553901fe5b3cd69644
Artur <artur.brandi@rwsretail.com>
2024-04-08 18:57:35 -0300

    Update users.bash

--- scripts/users.bash
+++ scripts/users.bash
@@ -1,8 +1,8 @@
 usersList() {
-  USER1="rws-integration-service, rws-sftp-test;"
-  USER2="rappi, rws-sftp-test;"
+  USER1="rws-integration-service,rws-sftp-test;"
+  USER2="rappi,rws-sftp-test;"
 
-  userList=($USER1 $USER2)
+  userList=($USER1$USER2)
 
   echo "${userList[@]}"
 }
