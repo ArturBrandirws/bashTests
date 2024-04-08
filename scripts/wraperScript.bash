6a4e5ffbe2be440f88e169c1741eb6c07003789a
Artur <artur.brandi@rwsretail.com>
2024-04-08 12:13:54 -0300

    Update wraperScript.bash

--- scripts/wraperScript.bash
+++ scripts/wraperScript.bash
@@ -1,29 +1,27 @@
 setNewEc2() {
 
     ## Configure AWSCLI
-    bash <(wget -qO- "https://raw.githubusercontent.com/ArturBrandirws/bashTests/main/scripts/configureAWSCLI.bash") $1 $2
+    bash <(wget -qO- "https://raw.githubusercontent.com/ArturBrandirws/bashTests/main/scripts/configureAWSCLI.bash")
 
     ## StoreAwsCredentials
-    bash <(wget -qO- "https://raw.githubusercontent.com/ArturBrandirws/bashTests/main/scripts/storeAwsCredentials.bash") $1 $2
+    bash <(wget -qO- "https://raw.githubusercontent.com/ArturBrandirws/bashTests/main/scripts/storeAwsCredentials.bash")
 
     ## mount bucket
-    bash <(wget -qO- "https://raw.githubusercontent.com/ArturBrandirws/bashTests/main/scripts/s3Configuration.bash") $3 $4
+    bash <(wget -qO- "https://raw.githubusercontent.com/ArturBrandirws/bashTests/main/scripts/s3Configuration.bash") $1 $2
 
     ## create user
-    bash <(wget -qO- "https://raw.githubusercontent.com/ArturBrandirws/bashTests/main/scripts/userCreation.bash") $4 $5 $6
+    bash <(wget -qO- "https://raw.githubusercontent.com/ArturBrandirws/bashTests/main/scripts/userCreation.bash") $2 $3 $4
 }
 
 main() {
 
     ## assign params
-    access_key=$1
-    secret_key=$2
-    Bucket_Name=$3
-    mount_location=$4
-    group_user=$5
-    username=$6
+    Bucket_Name=$1
+    mount_location=$2
+    group_user=$3
+    username=$4
 
-    setNewEc2 "$1" "$2" "$3" "$4" "$5" "$6"
+    setNewEc2 "$1" "$2" "$3" "$4"
 }
 
 main "$@"
