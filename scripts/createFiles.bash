eead856d5ac945792b298e694ac6078827028f0d
Artur <artur.brandi@rwsretail.com>
2024-04-08 09:28:23 -0300

    Create createFiles.bash

---
+++ scripts/createFiles.bash
@@ -0,0 +1,17 @@
+createFiles2() {
+    # Create 10000 files.txt with different names
+    for ((i=1; i<=10000; i++)); do
+        touch "$1/$2/file_${i}.txt"
+    done
+}
+
+main() {
+
+    ## assign params
+    mount_location=$1
+    username=$2
+
+    createFiles "$1" "$2"
+}
+
+main "$@"
