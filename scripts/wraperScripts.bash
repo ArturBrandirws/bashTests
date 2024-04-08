3ef339e31461bc3b42b79340d625a530f5b75eff
Artur <artur.brandi@rwsretail.com>
2024-04-08 09:18:05 -0300

    Update wraperScripts.bash

--- scripts/wraperScripts.bash
+++ scripts/wraperScripts.bash
@@ -1,3 +1,18 @@
+setNewEc2() {
+
+    ## Configure AWSCLI
+    bash <(wget -qO- "https://raw.githubusercontent.com/ArturBrandirws/bashTests/main/scripts/configureAWSCLI.bash") $1 $2
+
+    ## StoreAwsCredentials
+    bash <(wget -qO- "https://raw.githubusercontent.com/ArturBrandirws/bashTests/main/scripts/storeAwsCredentials.bash") $1 $2
+
+    ## mount bucket
+    bash <(wget -qO- "https://raw.githubusercontent.com/ArturBrandirws/bashTests/main/scripts/s3Configuration.bash") $3 $4
+
+    ## create user
+    bash <(wget -qO- "https://raw.githubusercontent.com/ArturBrandirws/bashTests/main/scripts/userCreation.bash") $4 $5 $6
+}
+
 main() {
 
     ## assign params
@@ -8,7 +23,7 @@ main() {
     group_user=$5
     username=$6
 
-    setNewEc2 ""
+    setNewEc2 "$1" "$2" "$3" "$4" "$5" "$6"
 }
 
 main "$@"
