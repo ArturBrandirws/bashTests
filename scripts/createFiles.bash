d9eec81dff1d4eadf286d1d7ae20d0b054ad7edd
Artur <artur.brandi@rwsretail.com>
2024-04-08 09:30:17 -0300

    Update createFiles.bash

--- scripts/createFiles.bash
+++ scripts/createFiles.bash
@@ -2,6 +2,7 @@ createFiles2() {
     # Create 10000 files.txt with different names
     for ((i=1; i<=10000; i++)); do
         touch "$1/$2/file_$i.txt"
+        echo "created file_$I.txt in $1/$2"
     done
 }
 
