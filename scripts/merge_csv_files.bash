d7d282f57cadeb1cbfbf541ea4a56654cdc32c9a
Artur <artur.brandi@rwsretail.com>
2024-05-08 15:28:34 -0300

    Update merge_csv_files.bash

--- scripts/merge_csv_files.bash
+++ scripts/merge_csv_files.bash
@@ -1,13 +1,31 @@
 #!/bin/bash
 
-# Define functions
-
 # Function to download CSV files from S3
 download_csv_files() {
-    local bucket="$1"
-    local folder_path="$2"
-    echo "Downloading CSV files from S3..."
-    aws s3 sync "s3://$bucket/$folder_path" ./temp_files --exclude "*" --include "*.csv"
+    local origin_path="$1"
+
+    if [[ "$origin_path" == s3://* ]]; then
+        # Destination is S3 path
+
+        # Define params for the function
+        local bucket_and_path="${origin_path#s3://}"
+        local bucket="${bucket_and_path%%/*}"
+        local s3_folder_path="${bucket_and_path#"$bucket"}"
+
+        echo "Downloading CSV files from S3..."
+        aws s3 sync "s3://$bucket/$s3_folder_path" ./temp_files --exclude "*" --include "*.csv"
+    else
+        # Destination is local path
+        echo "Copying CSV files from local path..."
+
+        # Check if origin_path ends with a slash
+        if [[ "${origin_path: -1}" != "/" ]]; then
+            origin_path="$origin_path/"
+        fi
+
+        # Copy all CSV files from origin_path to ./temp_files
+        cp -v "$origin_path"*.csv ./temp_files/
+    fi
 }
 
 # Function to merge CSV files into a single file with new lines between files
@@ -19,28 +37,50 @@ merge_csv_files() {
     # Loop through downloaded CSV files
     for file in ./temp_files/*.csv; do
         if [ "$first_file" = true ]; then
-            # If it's the first file, copy the header to the final file
-            head -n 1 "$file" > "$output_file_name"
+            # If it's the first file, copy the entire file content to the final file
+            cat "$file" > "$output_file_name"
             first_file=false
         else
-            # Insert a blank line to separate files
-            echo "" >> "$output_file_name"
+            # Append all lines (excluding header) to the final file
+            tail -n +2 "$file" >> "$output_file_name"
         fi
-        # Append all lines (excluding header) to the final file
-        tail -n +2 "$file" >> "$output_file_name"
     done
 }
 
-# Function to move the final file to S3
-move_file_to_s3() {
-    local output_file_name="$1"
-    local bucket="$2"
-    local destination_path="$3"
-    echo "Moving final file to S3..."
-    aws s3 cp "./$output_file_name" "s3://$bucket/$destination_path/$output_file_name"
+# Function to move the final file to S3 or keep it locally
+move_file_to_destination() {
+    local destination_path="$1"
+    local output_file_name
+
+    if [[ "$destination_path" == *.csv ]]; then
+        # User specified the name of the file in destination_path
+        output_file_name="${destination_path##*/}"  # Get the file name part from the path
+    else
+        # Default output file name if not specified by the user
+        output_file_name="merged_file.csv"
+    fi
+
+    echo "Moving final file to destination..."
+
+    if [[ "$destination_path" == s3://* ]]; then
+        # Destination is S3 path
+
+        # Define params for the function
+        local bucket_and_path="${destination_path#s3://}"
+        local bucket="${bucket_and_path%%/*}"
+        local s3_destination_path="${bucket_and_path#"$bucket"}"
+
+        echo "Moving final file to S3 $bucket"
+        aws s3 cp "./$output_file_name" "s3://$bucket$s3_destination_path/$output_file_name"
+    else
+        # Destination is local path
+        mkdir -p "/$destination_path"
+        echo "Moving final file to local ./$destination_path/$output_file_name"
+        mv "./$output_file_name" "./$destination_path/$output_file_name"
+    fi
 }
 
-# Function to clean up temporary files
+# Function to clean up temporary file
 cleanup_temp_files() {
     echo "Cleaning up temporary files..."
     rm -rf ./temp_files
@@ -48,21 +88,19 @@ cleanup_temp_files() {
 
 main() {
     # Check if all required parameters are provided
-    if [ "$#" -ne 4 ]; then
-        echo "Usage: $0 <bucket> <folder_path> <output_file_name> <destination_path>"
+    if [ "$#" -ne 2 ]; then
+        echo "Usage: $0 <origin_path> <destination_path>"
         exit 1
     fi
 
-    # Parse command-line arguments
-    bucket="$1"
-    folder_path="$2"
-    output_file_name="$3"
-    destination_path="$4"
+    # set params
+    origin_path="$1"
+    destination_path="$2"
 
     # Execute functions with provided parameters
-    download_csv_files "$bucket" "$folder_path"
+    download_csv_files "$origin_path"
     merge_csv_files "$output_file_name"
-    move_file_to_s3 "$output_file_name" "$bucket" "$destination_path"
+    move_file_to_destination "$output_file_name" "$destination_path"
     cleanup_temp_files
 
     echo "Process completed!"
